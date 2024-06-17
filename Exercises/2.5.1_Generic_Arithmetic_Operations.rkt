7f9d994b43ce389bb4387fa7d0054aaa6b94ca72
holmen1 <holmen1@gmail.com>
2024-06-17 20:09:42 +0200

    add rational-package

--- Exercises/2.5.1_Generic_Arithmetic_Operations.rkt
+++ Exercises/2.5.1_Generic_Arithmetic_Operations.rkt
@@ -14,9 +14,9 @@
   (let ((type-tags (map type-tag args)))
     (let ((proc (get op type-tags)))
       (if proc
-              (apply proc (map contents args))
-              (error "No method for these types: APPLY-GENERIC"
-                     (list op type-tags))))))
+        (apply proc (map contents args))
+        (error "No method for these types: APPLY-GENERIC"
+                (list op type-tags))))))
 
 ;; Define specific arithmetic operations using the generic apply-generic function
 (define (add x y) (apply-generic 'add x y))
@@ -26,46 +26,90 @@
 
 ;; Define a function to attach a type tag to a value
 (define (attach-tag type-tag contents)
-        (cons type-tag contents))
+  (cons type-tag contents))
 
 ;; Define a function to retrieve the type tag from a tagged datum
 (define (type-tag datum)
-        (if (pair? datum)
-                (car datum)
-                (error "Bad tagged datum: TYPE-TAG" datum)))
+  (if (pair? datum)
+    (car datum)
+    (error "Bad tagged datum: TYPE-TAG" datum)))
 
 ;; Define a function to retrieve the contents from a tagged datum
 (define (contents datum)
-        (if (pair? datum)
-                (cdr datum)
-                (error "Bad tagged datum: CONTENTS" datum)))
+  (if (pair? datum)
+    (cdr datum)
+    (error "Bad tagged datum: CONTENTS" datum)))
 
-;; Define a function to install the scheme-number package
+;; scheme-number package
 (define (install-scheme-number-package)
-        (define (tag x) (attach-tag 'scheme-number x))
-        (put 'add '(scheme-number scheme-number)
-                (lambda (x y) (tag (+ x y))))
-        (put 'sub '(scheme-number scheme-number)
-                (lambda (x y) (tag (- x y))))
-        (put 'mul '(scheme-number scheme-number)
-                (lambda (x y) (tag (* x y))))
-        (put 'div '(scheme-number scheme-number)
-                (lambda (x y) (tag (/ x y))))
-        (put 'make 'scheme-number (lambda (x) (tag x)))
-        'done)
+  (define (tag x) (attach-tag 'scheme-number x))
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number (lambda (x) (tag x)))
+  'done)
 
 ;; Define a function to create a scheme-number
 (define (make-scheme-number n)
-        ((get 'make 'scheme-number) n))
+  ((get 'make 'scheme-number) n))
 
 ;; Test the add-scheme-number function
 (install-scheme-number-package)
 
 (define (add-scheme-number x y)
-        (apply-generic 'add x y))
+  (apply-generic 'add x y))
 
 (add-scheme-number (make-scheme-number 5) (make-scheme-number 6)) ;'(scheme-number . 11)
 
 
+;; rational-package
+(define (install-rational-package)
+  ;; internal procedures
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+  (define (make-rat n d)
+    (let ((g (gcd n d)))
+      (cons (/ n g) (/ d g))))
+  (define (add-rat x y)
+    (make-rat (+ (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (sub-rat x y)
+    (make-rat (- (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (mul-rat x y)
+    (make-rat (* (numer x) (numer y))
+              (* (denom x) (denom y))))
+  (define (div-rat x y)
+    (make-rat (* (numer x) (denom y))
+              (* (denom x) (numer y))))
+  ;; interface to rest of the system
+  (define (tag x) (attach-tag 'rational x))
+  (put 'add '(rational rational)
+    (lambda (x y) (tag (add-rat x y))))
+  (put 'sub '(rational rational)
+    (lambda (x y) (tag (sub-rat x y))))
+  (put 'mul '(rational rational)
+    (lambda (x y) (tag (mul-rat x y))))
+  (put 'div '(rational rational)
+    (lambda (x y) (tag (div-rat x y))))
+  (put 'make 'rational
+    (lambda (n d) (tag (make-rat n d))))
+  'done)
 
+(define (make-rational n d)
+  ((get 'make 'rational) n d))
 
+;; Test the add-scheme-number function
+(install-rational-package)
+
+(define (add-rational-number x y)
+  (apply-generic 'add x y))
+
+(add-rational-number (make-rational 1 3) (make-rational 2 3)) ;'(rational 1 . 1)
