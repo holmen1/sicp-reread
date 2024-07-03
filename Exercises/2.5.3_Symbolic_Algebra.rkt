eeff6f7d0b44d99493b1d5925acda8882560de41
holmen1 <holmen1@gmail.com>
2024-07-03 21:28:25 +0200

    Add add

--- Exercises/2.5.3_Symbolic_Algebra.rkt
+++ Exercises/2.5.3_Symbolic_Algebra.rkt
@@ -2,6 +2,9 @@
 
 (require "polynomial.rkt")
 (require "arithmetic_operations.rkt")
+(require "number.rkt")
+
+(install-scheme-number-package)
 ;(require "utils.rkt") ; for debug
 
 
@@ -11,8 +14,12 @@
 ;             (printf "~a\n" pair))
 ;           (hash->list operations))
 
-(make-polynomial 'x '((2 0))) ;'(polynomial x (2 0))
+(make-polynomial 'x '((2 0)))             ;'(polynomial x (2 0))
+(make-polynomial 'x '((2 0) (1 1) (0 6))) ;'(polynomial x (2 0) (1 1) (0 6))
 
 (=zero? (make-polynomial 'x '())) ;#t
 (=zero? (make-polynomial 'x '((2 0)))) ;#t
 (=zero? (make-polynomial 'x '((2 1) (1 0)))) ;#f
+
+(add '(polynomial x (2 2)) '(polynomial x (2 3))) ;'(polynomial x (2 5))
+(add '(polynomial x (2 0) (1 1) (0 6)) '(polynomial x (2 0) (1 1) (0 6))) ;'(polynomial x (1 2) (0 12))
