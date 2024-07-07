f313461f910d212260a36028d41560196dd7f0a4
holmen1 <holmen1@gmail.com>
2024-07-07 16:02:35 +0200

    Exercise 3.8

--- 3_Modularity_Objects_and_State/3.1.3_The Cost_of_Introducing_Assignment.rkt
+++ 3_Modularity_Objects_and_State/3.1.3_The Cost_of_Introducing_Assignment.rkt
@@ -55,3 +55,32 @@ name paul-acc and the password rosebud|#
 ((paul-acc 'rosebud 'withdraw) 10) ;=> 80
 ((peter-acc 'open-sesame 'withdraw) 10) ;=> 70
 ((peter-acc 'sesame 'withdraw) 10) ;=> "Incorrect password"
+
+
+#|Exercise 3.8
+When we defined the evaluation model in Section 1.1.3, we said that the first step in evaluating an
+expression is to evaluate its subexpressions. But we never specified the order in which the sub-
+expressions should be evaluated (e.g., left to right or right to left). When we introduce assignment,
+the order in which the arguments to a procedure are evaluated can make a difference to the result.
+Define a simple procedure f such that evaluating
+
+(+ (f 0) (f 1))
+
+will return 0 if the arguments to + are evaluated from left to right but will return 1 if the arguments
+are evaluated from right to left|#
+
+(define f
+  (let ((init 1))
+    (lambda (incr)
+            (begin (set! init (* init incr))
+                   init))))
+
+(define f2
+  (let ((init 1))
+    (lambda (incr)
+            (begin (set! init (* init incr))
+                   init))))
+
+
+(+ (f 0) (f 1)) ; => 0, revealing this racket + evaluates left to right
+(+ (f2 1) (f2 0)) ; => 1
