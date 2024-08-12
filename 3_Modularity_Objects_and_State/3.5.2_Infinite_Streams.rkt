2938c68e6f19e7299de8126ea7476ded1497bc00
holmen1 <holmen1@gmail.com>
2024-08-12 19:52:30 +0200

    Exercise 3.55

--- 3_Modularity_Objects_and_State/3.5.2_Infinite_Streams.rkt
+++ 3_Modularity_Objects_and_State/3.5.2_Infinite_Streams.rkt
@@ -123,3 +123,19 @@ definition of the stream whose nth element (counting from 0) is n + 1 factorial:
 
 ;; test
 (stream-take factorials 6); => '(1 1 2 6 24 120)
+
+
+
+#|Exercise 3.55
+Define a procedure partial-sums that takes as argument a stream S and returns the stream whose elements
+are S0, S0 + S1, S0 + S1 + S2 , . . ..
+For example, (partial-sums integers) should be the stream 1, 3, 6, 10, 15, . . .
+|#
+
+(define (partial-sums s)
+  (cons-stream (stream-car s)
+               (add-streams (partial-sums s)
+                            (stream-cdr s))))
+
+;; test
+(stream-take (partial-sums factorials) 5); => '(1 2 4 10 34)
