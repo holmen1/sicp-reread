d86aab82c708085dd888f867106e8ef58d9ebd24
holmen1 <holmen1@gmail.com>
2024-08-12 19:34:45 +0200

    Exercise 3.54

--- 3_Modularity_Objects_and_State/3.5.2_Infinite_Streams.rkt
+++ 3_Modularity_Objects_and_State/3.5.2_Infinite_Streams.rkt
@@ -106,3 +106,20 @@
 
 (stream-take primes-i 10) ; => '(2 3 5 7 11 13 17 19 23 29)
 (stream-ref primes-i 50) ; => 233
+
+
+#|Exercise 3.54
+Define a procedure mul-streams, analogous to add-streams, that produces the elementwise product of
+its two input streams. Use this together with the stream of integers to complete the following
+definition of the stream whose nth element (counting from 0) is n + 1 factorial:
+|#
+
+(define (mul-streams s1 s2) (stream-map * s1 s2))
+
+(define factorials
+  (cons-stream 1
+               (mul-streams factorials
+                            integers)))
+
+;; test
+(stream-take factorials 6); => '(1 1 2 6 24 120)
