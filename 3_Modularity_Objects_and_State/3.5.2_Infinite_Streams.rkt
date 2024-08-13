5ff4ed45f7d5779eac2c050b96a7fb681616b800
holmen1 <holmen1@gmail.com>
2024-08-13 14:45:23 +0200

    Exercise 3.59

--- 3_Modularity_Objects_and_State/3.5.2_Infinite_Streams.rkt
+++ 3_Modularity_Objects_and_State/3.5.2_Infinite_Streams.rkt
@@ -182,3 +182,37 @@ combines two ordered streams into one ordered result stream, eliminating repetit
 (stream-take S 12); => '(1 2 3 4 5 6 8 9 10 12 15 16)
 
 
+
+#|Exercise 3.59
+In Section 2.5.3 we saw how to implement a polynomial arithmetic system representing polynomials
+as lists of terms. In a similar way, we can work with powerseries represented as infinite streams.
+We will represent the series a0 + a1x + a2x2 + a3x3 + . . . as the stream whose elements are the
+coefficients a0, a1, a2, a3, . . .
+
+a. The integral of the series a0 + a1x + a2x2 + a3x3 + . . . is the series c + a0x + 1/2a1x2 + 1/3a2x3 + 1/4a3x4 + . . . ,
+where c is any constant. Define a procedure integrate-series that takes as input a stream a0, a1, a2, . . .
+representing a power series and returns the stream a0, 1/2 a1, 1/3 a2, . . . of coefficients of the
+non-constant terms of the integral of the series. (Since the result has no constant term, it doesn’t
+represent a power series; when we use integrate-series, we will cons on the appropriate constant.)|#
+
+(define (integrate-series S)
+  (stream-map / S
+                integers))
+
+;; test
+(stream-take
+  (integrate-series (integers-starting-from 10))
+  6)
+; => '(10 11/2 4 13/4 14/5 5/2)
+
+#|b. The function x -> e^x is its own derivative. This implies that e^x and the integral of e^x are the
+same series, except for the constant term, which is e0 = 1. Accordingly, we can generate the series for
+e^x as |#
+(define exp-series
+  (cons-stream 1 (integrate-series exp-series)))
+
+;; Show how to generate the series for sine and cosine, starting from the facts that the derivative of sine is cosine and the derivative of cosine is the negative of sine:
+
+(define cosine-series (cons-stream 1 (integrate-series sine-series)))
+(define sine-series (cons-stream 0 (scale-stream (integrate-series cosine-series)
+                                                 -1)))
